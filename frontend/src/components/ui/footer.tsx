ff3264ab6649f2ec3207e30537f0a59ed540a5c4
ice28992 <ice28992@gmail.com>
2025-09-20 21:17:02 +0900

    fix: footerの修正

--- frontend/src/components/ui/footer.tsx
+++ frontend/src/components/ui/footer.tsx
@@ -6,9 +6,9 @@ export default function Footer() {
     <Box
       component="footer"
       sx={{
-        position: "fixed",    // 画面下に固定
+        // position: "fixed",    // 画面下に固定
         bottom: 0,
-        width: "53.7%",       // 横幅いっぱいに修正
+        width: "100%",       // 横幅いっぱいに修正
         height: 60,           // 高さ固定
         background: "linear-gradient(90deg, #FFC107 0%, #FFF176 100%)",
         display: "flex",
