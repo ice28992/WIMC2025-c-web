2aab417a08ef3e136abbdca56d18c8f9d2e9f90a
ice28992 <ice28992@gmail.com>
2025-09-21 00:56:21 +0900

    fix

--- frontend/eslint.config.mjs
+++ frontend/eslint.config.mjs
@@ -10,8 +10,13 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
+  // Next.jsとTypeScriptの設定を拡張
   ...compat.extends("next/core-web-vitals", "next/typescript"),
   {
+    rules: {
+      // TypeScript関連のルールを設定
+      "@typescript-eslint/no-explicit-any": "off", // `any` 型を許可
+    },
     ignores: [
       "node_modules/**",
       ".next/**",
