124b8c4c68882e8470d5cc2d636fe46e93a9eafd
ice28992 <ice28992@gmail.com>
2025-09-21 12:20:22 +0900

    feat: next.confg編集

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,7 +1,15 @@
 // next.config.js
 /** @type {import('next').NextConfig} */
+const withPWA = require('next-pwa');
+
 const nextConfig = {
-  // 設定内容
+  reactStrictMode: true,
+  // PWAの設定
+  pwa: {
+    dest: 'public', // Service Worker と関連ファイルを出力する場所
+    register: true,  // Service Worker を登録するかどうか
+    skipWaiting: true,  // 新しい Service Worker がインストールされた時、即座にページを更新するかどうか
+  },
 };
 
-module.exports = nextConfig;
+module.exports = withPWA(nextConfig);
